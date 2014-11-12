deee902939a5f3c3f1f86220aa0c7e03953726df
tgdriro5 <ronald.rink@d-fens.net>
2014-11-12 03:14:54 +0100

    ADD: initial commit

---
+++ SimpleStateMachine/Program.cs
@@ -0,0 +1,50 @@
+﻿// Based on an idea by "Juliet" (http://stackoverflow.com/users/40516/juliet)
+// http://stackoverflow.com/questions/5923767/simple-state-machine-example-in-c
+
+using System;
+
+namespace SimpleStateMachine
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Execute standard process
+            var p = new ProcessBase();
+            Console.WriteLine(p.GetStateMachine()); 
+            while (!p.IsFinalState)
+            {
+                Console.WriteLine("Previous State = " + p.PreviousState);
+                Console.WriteLine("Current State = " + p.CurrentState);
+                Console.WriteLine("Condition.Begin: Current State = " + p.Continue());
+            }
+
+            // Load a FSM and save its configuration ...
+            var p2 = new StatusEnum();
+            Console.WriteLine(p2.GetStateMachine());
+            var config = p2.GetStateMachine();
+
+            // ... and load it into a new FSM instance.
+            var p3 = new StatusEnum();
+            p3.SetStateMachine(config);
+            Console.WriteLine(p3.GetStateMachine());
+
+            // Modulate command state machine from Juliet
+            var cmd = new ProcessCommand();
+            Console.WriteLine(cmd.GetStateMachine());
+            Console.WriteLine("Previous State = " + cmd.PreviousState);
+            Console.WriteLine("Current State = " + cmd.CurrentState);
+            Console.WriteLine("Condition.Continue: Current State = " + cmd.Continue());
+            Console.WriteLine("Previous State = " + cmd.PreviousState);
+            Console.WriteLine("Condition.Cancel: Current State = " + cmd.Cancel());
+            Console.WriteLine("Previous State = " + cmd.PreviousState);
+            Console.WriteLine("Condition.Cancel: Current State = " + cmd.Cancel());
+            Console.WriteLine("Previous State = " + cmd.PreviousState);
+            Console.WriteLine("Condition.Cancel: Current State = " + cmd.Cancel());
+            Console.WriteLine("Previous State = " + cmd.PreviousState);
+            Console.WriteLine("IsFinalState = " + cmd.IsFinalState);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleStateMachine/StatusEnum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleStateMachine
+{
+    public class StatusEnum : ProcessBase
+    {
+        public StatusEnum()
+        {
+            InsertStateTransition(InitialState, ContinueCondition, WatingState, true);
+            SetStateTransition(WatingState, CancelCondition, CancelledState);
+        }
+        public override string InitialState
+        {
+            get
+            {
+                return "Pending";
+            }
+        }
+        public string WatingState
+        {
+            get
+            {
+                return "Waiting";
+            }
+        }
+        public override string CancelledState
+        {
+            get
+            {
+                return "Failed";
+            }
+        }
+    }
+}
